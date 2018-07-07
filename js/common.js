b19cbaa865abb6f6858e47f4d2dd83ae61208b93
Maksym Koldun <koldun.maks@gmail.com>
2018-07-07 13:41:01 +0300

    adding new mediaQueries

--- js/common.js
+++ js/common.js
@@ -1,14 +1,13 @@
 
 var openBtnNav = document.getElementsByClassName('hamburger-menu')[0];
 var closeBtnNav = document.getElementsByClassName('closebtn')[0];
-var shiftsTitle = document.getElementsByClassName('shift-evening-title');
+var shiftsTitle = document.getElementsByClassName('title-shift');
 
-console.log(shiftsTitle[0]);
 
-shiftsTitle[0].addEventListener("click", () => {
-	alert();
-});
-
+for(let i = 0; i < shiftsTitle.length; i++){
+	console.log(shiftsTitle[i]);
+	shiftsTitle[i].addEventListener("click", setPropertyTitleShift);
+}
 
 openBtnNav.addEventListener("click", openNav);
 closeBtnNav.addEventListener("click", closeNav);
@@ -19,4 +18,12 @@ function openNav() {
 
 function closeNav() {
     document.getElementsByClassName("sidenav")[0].style.width = "0";
+}
+
+function setPropertyTitleShift(e) {
+	console.log(i);
+	e.style.backgroundColor = "rgb(255,140,0)";
+	e.style.transform = "translateY(5px)";
+    e.style.boxShadow = "0px 3px rgba(0,0,0,.9)";
+    e.style.color = "black";
 }
